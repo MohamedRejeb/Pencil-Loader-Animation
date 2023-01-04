0625f3c912abded1589ef1ac09d11b8d301cc502
M0Coding <mohamedrejeb445@gmail.com>
2023-01-04 18:07:17 +0100

    Adding basic loading animation

--- app/src/main/java/com/mohamedbenrejeb/pencilloader/PencilLoader.kt
+++ app/src/main/java/com/mohamedbenrejeb/pencilloader/PencilLoader.kt
@@ -1,11 +1,13 @@
 package com.mohamedbenrejeb.pencilloader
 
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
@@ -16,21 +18,36 @@ import androidx.compose.ui.unit.dp
 fun PencilLoader(
     modifier: Modifier = Modifier
 ) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val progress by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
 
     Box(modifier = modifier) {
         CircularProgressIndicator(
             modifier = Modifier
                 .graphicsLayer {
-                    rotationZ = 45f
+                    rotationZ = 0f
                 }
                 .fillMaxSize()
             ,
-            progress = 0.6f,
+            progress = progress,
             strokeWidth = 10.dp,
             color = Color(0xff192c55)
         )
 
-        Pencil(modifier = Modifier.fillMaxSize())
+        Pencil(
+            modifier = Modifier
+                .graphicsLayer {
+                    rotationZ = 360 * progress
+                }
+                .fillMaxSize()
+        )
     }
 
 }
